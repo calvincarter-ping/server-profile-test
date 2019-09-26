047fd16d87131de801b8b4d900e9fcc6f19e40b6
Calvin Carter <calvincarter@pingidentity.com>
2019-09-26 15:58:04 -0500

    add pa.jwk check

--- pingaccess/hooks/81-after-start-process.sh
+++ pingaccess/hooks/81-after-start-process.sh
@@ -6,6 +6,9 @@ run_hook "83-create-initial-password.sh"
 
 echo "Checking for data.json to import.."
 
+if ! test -f "${STAGING_DIR}/instance/conf/pa.jwk" ; then
+  echo "INFO: No file named /instance/conf/pa.jwk found"
+fi
 if ! test -f "${STAGING_DIR}/instance/data/data.json" ; then
   echo "INFO: No file named /instance/data/data.json found"
 fi
