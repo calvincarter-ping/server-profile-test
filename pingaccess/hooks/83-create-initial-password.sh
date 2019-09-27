e8dd3cb2ca6ea21bed037a594d311d71e61fbcab
Calvin Carter <calvincarter@pingidentity.com>
2019-09-27 16:23:32 -0500

    latest server profile

--- pingaccess/hooks/83-create-initial-password.sh
+++ pingaccess/hooks/83-create-initial-password.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env sh
 . "${HOOKS_DIR}/pingcommon.lib.sh"
 
+host=`hostname`
+
+echo "host cal ${host}"
+
 curl -k -X PUT -u Administrator:2Access --silent -H "X-Xsrf-Header: PingAccess" -d '{ "email": null,
     "slaAccepted": true,
     "firstLogin": false,
     "showTutorial": false,
     "username": "Administrator"
-}' https://localhost:9000/pa-admin-api/v3/users/1 > /dev/null
+}' "https://${host}:9000/pa-admin-api/v3/users/1" > /dev/null
 
 # echo "INFO: changing initial password"
 curl -k -X PUT -u Administrator:2Access --silent -H "X-Xsrf-Header: PingAccess" -d '{
   "currentPassword": "2Access",
   "newPassword": "'"${INITIAL_ADMIN_PASSWORD}"'"
-}' https://localhost:9000/pa-admin-api/v3/users/1/password > /dev/null
+}' "https://${host}:9000/pa-admin-api/v3/users/1/password" > /dev/null
