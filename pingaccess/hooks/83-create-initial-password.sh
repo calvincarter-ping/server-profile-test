5c46ee2b9e5f68605689c08c2fa0e46502678de5
Calvin Carter <calvincarter@pingidentity.com>
2019-10-01 23:46:34 -0500

    json fix

--- pingaccess/hooks/83-create-initial-password.sh
+++ pingaccess/hooks/83-create-initial-password.sh
@@ -16,18 +16,18 @@ curl -k -X PUT -u Administrator:2Access --silent -H "X-Xsrf-Header: PingAccess" 
   "newPassword": "'"${INITIAL_ADMIN_PASSWORD}"'"
 }' https://localhost:9000/pa-admin-api/v3/users/1/password > /dev/null
 
-# Update admin config host
-curl -k -X PUT -u Administrator:${INITIAL_ADMIN_PASSWORD} -H "X-Xsrf-Header: PingAccess" -d "{
-        \"hostPort\": \"localhost:9090\",
-        \"httpProxyId\": 0,
-        \"httpsProxyId\": 0
-}" https://localhost:9000/pa-admin-api/v3/adminConfig > /dev/null
-
 curl -k -X POST -u Administrator:${INITIAL_ADMIN_PASSWORD} -H "X-Xsrf-Header: PingAccess" -d "{
         \"name\": \"${PA_CONSOLE_HOST}\",
         \"host\": \"${PA_CONSOLE_HOST}\",
         \"port\": \"9090\"
 }" https://localhost:9000/pa-admin-api/v3/proxies > /dev/null
+
+# Update admin config host
+curl -k -X PUT -u Administrator:${INITIAL_ADMIN_PASSWORD} -H "X-Xsrf-Header: PingAccess" -d "{
+        \"hostPort\": \"localhost:9090\",
+        \"httpProxyId\":1,
+        \"httpsProxyId\":1
+}" https://localhost:9000/pa-admin-api/v3/adminConfig > /dev/null
 
 #echo "importing data"
 #curl -k -v -X POST -u Administrator:${INITIAL_ADMIN_PASSWORD} -H "Content-Type: application/json" -H "X-Xsrf-Header: #PingAccess" 
