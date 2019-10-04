60295ff862fde45ce38e2adccfeaccb76a45a4ab
Calvin Carter <calvincarter@pingidentity.com>
2019-10-04 15:45:33 -0500

    latest update

--- pingaccess/hooks/83-create-initial-password.sh
+++ pingaccess/hooks/83-create-initial-password.sh
@@ -37,8 +37,6 @@ OUT=$( make_api_request -X POST -d "{
 paEngineKeyPairId=$( jq -n "$OUT" | jq '.id' )
 echo "EngineKeyPairId:"${paEngineKeyPairId}
 
-make_api_request -X DELETE https://${PA_CONSOLE_HOST}:9000/pa-admin-api/v3/keyPairs/2
-
 # Retrieving CONFIG QUERY id
 OUT=$( make_api_request https://${PA_CONSOLE_HOST}:9000/pa-admin-api/v3/httpsListeners )
 configQueryListenerId=$( jq -n "$OUT" | jq '.items[] | select(.name=="CONFIG QUERY") | .id' )
@@ -50,3 +48,5 @@ make_api_request -X PUT -d "{
     \"useServerCipherSuiteOrder\": true,
     \"keyPairId\": ${paEngineKeyPairId}
 }" https://${PA_CONSOLE_HOST}:9000/pa-admin-api/v3/httpsListeners/${configQueryListenerId} > /dev/null
+
+make_api_request -X DELETE https://${PA_CONSOLE_HOST}:9000/pa-admin-api/v3/keyPairs/2
