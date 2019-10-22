eb107d34653e619d6a16cb207254673e219349c8
Calvin Carter <calvincarter@pingidentity.com>
2019-10-22 06:06:30 -0500

    troubleshoot

--- pingaccess/hooks/utils.lib.sh
+++ pingaccess/hooks/utils.lib.sh
@@ -6,16 +6,14 @@ APIATTEMPTS=10
 
 function make_api_request
 {
-    echo password ${INITIAL_ADMIN_PASSWORD}
-
     local retryAttempts=${APIATTEMPTS}
     while true; do
-    curl -k -u Administrator:2FederateM0re -H "X-Xsrf-Header: PingAccess " "$@"
+    curl -k --silent -u Administrator:2FederateM0re -H "X-Xsrf-Header: PingAccess " "$@"
     if [[ ! $? -eq 0 && $retryAttempts -gt 0 ]]; then
         retryAttempts=$((retryAttempts-1))
         sleep 3
     elif [ $retryAttempts -eq 0 ]; then
-        exit 1
+        return 1
     else
         break
     fi
