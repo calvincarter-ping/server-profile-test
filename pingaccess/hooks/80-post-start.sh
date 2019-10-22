265886aa3c7144ec560f1907eaa7847e83503e25
Calvin Carter <calvincarter@pingidentity.com>
2019-10-22 10:06:47 -0500

    troubleshoot

--- pingaccess/hooks/80-post-start.sh
+++ pingaccess/hooks/80-post-start.sh
@@ -7,8 +7,23 @@
 
 # shellcheck source=pingcommon.lib.sh
 . "${HOOKS_DIR}/pingcommon.lib.sh"
-
-if test -z "${OPERATIONAL_MODE}" || test "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE"  ; then
-  echo "INFO: waiting for PingAccess to start before importing configuration"
-  wait-for localhost:9000 -t 200 --  "${HOOKS_DIR}/81-after-start-process.sh"
+INITIAL_ADMIN_PASSWORD=${INITIAL_ADMIN_PASSWORD:=2FederateM0re}
+if test ${RUN_PLAN} = "START" ; then
+  echo "Check for configuration to import.."
+  if ! test -f ${STAGING_DIR}/instance/conf/pa.jwk ; then
+    echo "INFO: No 'pa.jwk' found in /instance/conf"
+    if ! test -f ${STAGING_DIR}/instance/data/data.json ; then
+      echo "INFO: No file named 'data.json' found in /instance/data"
+      echo "INFO: skipping config import"
+    fi
+  else 
+    if [[ ! -z "${OPERATIONAL_MODE}" && "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE" ]]; then
+      run_hook "81-import-initial-configuration.sh"
+    fi   
+  fi
 fi
+
+if [[ ! -z "${OPERATIONAL_MODE}" && "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE" ]]; then
+  echo "Bringing eth0 back up..."
+  ip link set eth0 up
+fi 
