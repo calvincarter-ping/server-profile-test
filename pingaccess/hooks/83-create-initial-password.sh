b57a90fefb4f3931a11649fecf061016776e9cda
Calvin Carter <calvincarter@pingidentity.com>
2019-10-22 09:37:36 -0500

    troubleshoot

--- pingaccess/hooks/83-create-initial-password.sh
+++ pingaccess/hooks/83-create-initial-password.sh
@@ -48,9 +48,3 @@ make_api_request -X PUT -d "{
     \"useServerCipherSuiteOrder\": false,
     \"keyPairId\": ${paEngineKeyPairId}
 }" https://localhost:9000/pa-admin-api/v3/httpsListeners/${configQueryListenerId}
-
-ip link set eth0 down
-
-sleep 10
-
-ip link set eth0 up
