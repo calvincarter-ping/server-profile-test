9a2ea41b1df8d13186be68d8cd72d2e8f126fff3
Calvin Carter <calvincarter@pingidentity.com>
2019-10-25 09:02:21 -0500

    removed engine

--- pingaccess/cluster-5.3.9.0/hooks/utils.lib.sh
+++ pingaccess/cluster-5.3.9.0/hooks/utils.lib.sh
@@ -2,20 +2,24 @@
 
 . "${HOOKS_DIR}/pingcommon.lib.sh"
 
-APIATTEMPTS=10
+#APIATTEMPTS=10
+# --max-time 10     (how long each retry will wait)
+# --retry 5         (it will retry 5 times)
+# --retry-delay 0   (an exponential backoff algorithm)
+# --retry-max-time  (total time before it's considered failed)
 
-function make_api_request
-{
-    local retryAttempts=10
-    while true; do
-        curl -k -u Administrator:2FederateM0re -H "X-Xsrf-Header: PingAccess " "$@"
-        if [[ ! $? -eq 0 && $retryAttempts -gt 0 ]]; then
-            retryAttempts=$((retryAttempts-1))
-            sleep 3
-        elif [ $retryAttempts -eq 0 ]; then
-            return 1
-        else
-            break
-        fi
-    done
-}
+#function make_api_request
+#{
+#    local retryAttempts=10
+#    while true; do
+        curl -k --max-time 3 --retry 10 -u Administrator:2FederateM0re -H "X-Xsrf-Header: PingAccess " "$@"
+#        if [[ ! $? -eq 0 && $retryAttempts -gt 0 ]]; then
+#            retryAttempts=$((retryAttempts-1))
+#            sleep 3
+#        elif [ $retryAttempts -eq 0 ]; then
+#            return 1
+#        else
+#            break
+#        fi
+#    done
+#}
