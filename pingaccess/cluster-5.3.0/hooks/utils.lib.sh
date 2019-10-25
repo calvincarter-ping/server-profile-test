ccae89946b7b7c7679172efddb970f3a993d3136
Calvin Carter <calvincarter@pingidentity.com>
2019-10-25 13:25:54 -0500

    renamed folder 5.3.0

---
+++ pingaccess/cluster-5.3.0/hooks/utils.lib.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+. "${HOOKS_DIR}/pingcommon.lib.sh"
+
+function make_api_request
+{
+    local retryAttempts=10
+    while true; do
+        curl -k -u Administrator:2FederateM0re -H "X-Xsrf-Header: PingAccess " "$@"
+        if [[ ! $? -eq 0 && $retryAttempts -gt 0 ]]; then
+            retryAttempts=$((retryAttempts-1))
+            sleep 3
+        elif [ $retryAttempts -eq 0 ]; then
+            return 1
+        else
+            break
+        fi
+    done
+}
