254baa6a5565dcd91ab13e0d25111b65f37c8e06
calvincarter-ping <calvincarter@pingidentity.com>
2019-11-21 13:56:08 -0600

    testing engine GET

--- pingaccess/cluster-5.3.0/hooks/51-add-engine.sh
+++ pingaccess/cluster-5.3.0/hooks/51-add-engine.sh
@@ -54,6 +54,10 @@ if [[ ! -z "${OPERATIONAL_MODE}" && "${OPERATIONAL_MODE}" = "CLUSTERED_ENGINE" ]
     }" https://${pahost}:9000/pa-admin-api/v3/engines )
     engineId=$( jq -n "$OUT" | jq '.id' )
 
+
+    engineTEST=$( curl -k -X GET -u Administrator:2FederateM0re --silent -H "X-Xsrf-Header: PingAccess" 
+    https://pingaccess:9000/pa-admin-api/v3/engines | jq --arg host "${host}" '.items[] | select(.name==$host | .id' )
+
     # Download Engine Configuration
     echo "EngineId:"${engineId}
     echo "Retrieving the engine config"
