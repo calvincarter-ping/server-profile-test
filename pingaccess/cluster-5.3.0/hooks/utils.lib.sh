1353acf099be1fd61d285916e87798b829969cd7
calvincarter-ping <calvincarter@pingidentity.com>
2019-11-27 11:12:50 -0600

    curl retry flag in pingaccess

--- pingaccess/cluster-5.3.0/hooks/utils.lib.sh
+++ pingaccess/cluster-5.3.0/hooks/utils.lib.sh
@@ -8,16 +8,8 @@
 ########################################################################################################################
 function make_api_request
 {
-    local retryAttempts=10
-    while true; do
-        curl -k -u Administrator:${INITIAL_ADMIN_PASSWORD} -H "X-Xsrf-Header: PingAccess " "$@"
-        if [[ ! $? -eq 0 && $retryAttempts -gt 0 ]]; then
-            retryAttempts=$((retryAttempts-1))
-            sleep 3
-        elif [ $retryAttempts -eq 0 ]; then
-            return 1
-        else
-            break
-        fi
-    done
+    curl -k --retry 10  --retry-delay 1 --retry-connrefuse -u Administrator:${INITIAL_ADMIN_PASSWORD} -H "X-Xsrf-Header: PingAccess " "$@"
+    if [[ ! $? -eq 0 ]]; then
+        exit 1
+    fi
 }
