10a475cfce2f54bc8fc640f042df993a585488db
calvincarter-ping <calvincarter@pingidentity.com>
2019-11-27 14:31:59 -0600

    environment var for api limit

--- pingaccess/cluster-5.3.0/hooks/utils.lib.sh
+++ pingaccess/cluster-5.3.0/hooks/utils.lib.sh
@@ -8,7 +8,7 @@
 ########################################################################################################################
 function make_api_request
 {
-    curl -k --retry 10  --retry-delay 1 --retry-connrefuse -u Administrator:${INITIAL_ADMIN_PASSWORD} -H "X-Xsrf-Header: PingAccess " "$@"
+    curl -k --retry ${API_RETRY_LIMIT} --retry-delay 1 --retry-connrefuse -u Administrator:${INITIAL_ADMIN_PASSWORD} -H "X-Xsrf-Header: PingAccess " "$@"
     if [[ ! $? -eq 0 ]]; then
         exit 1
     fi
