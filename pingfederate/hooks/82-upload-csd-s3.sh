79044475b0415ac5ceeba551cc844158a1f17303
calvincarter-ping <calvincarter@pingidentity.com>
2019-12-23 10:32:33 -0600

    latest s3 upload

--- pingfederate/hooks/82-upload-csd-s3.sh
+++ pingfederate/hooks/82-upload-csd-s3.sh
@@ -2,12 +2,13 @@
 
 ${VERBOSE} && set -x
 
+# TODO: may need to uncomment the following. Does PF need?
 # Set PATH - since this is executed from within the server process, it may not have all we need on the path
 #export PATH="${PATH}:${SERVER_ROOT_DIR}/bin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:${JAVA_HOME}/bin"
 
 # Allow overriding the log archive URL with an arg
 test ! -z "${1}" && PF_ARCHIVE_URL="${1}"
-echo "Uploading to location ${LOG_ARCHIVE_URL}"
+echo "Uploading to location ${PF_ARCHIVE_URL}"
 
 # Install AWS CLI if the upload location is S3
 if test "${PF_ARCHIVE_URL#s3}" == "${PF_ARCHIVE_URL}"; then
@@ -20,23 +21,25 @@ elif ! which aws > /dev/null; then
   pip3 install --no-cache-dir --upgrade awscli
 fi
 
-FORMAT="+%d/%b/%Y:%H:%M:%S %z"
-NOW=$(date "${FORMAT}")
-AN_HOUR_AGO=$(date --date="@$(($(date +%s) - 3600))" "${FORMAT}")
+# Check for archive folder on server
+if test -d "${OUT_DIR}/instance/server/default/data/archive" ; then
+  
+  # cd into archive directory
+  cd "${OUT_DIR}/instance/server/default/data/archive"
 
-# cd into archive directory
-cd "${OUT_DIR}/server/default/data/archive"
+  PF_BACKUP_OUT=$(find . -name data\*zip -type f | sort | tail -1)
 
-CSD_OUT=$(find . -name data\*zip -type f | sort | tail -1)
+  # aws s3 cp calvin.txt "${PF_ARCHIVE_URL}/${DST_FILE}"
+  aws s3 cp ${PF_BACKUP_OUT} "${PF_ARCHIVE_URL}"
 
-# aws s3 cp calvin.txt "${PF_ARCHIVE_URL}/${DST_FILE}"
-aws s3 cp ${CSD_OUT} "${PF_ARCHIVE_URL}"
+  echo "Upload return code: ${?}"
 
+  # Remove the CSD file so it is doesn't fill up the server's filesystem.
+  #rm -f "${PF_BACKUP_OUT}"
 
-echo "Upload return code: ${?}"
-
-# Remove the CSD file so it is doesn't fill up the server's filesystem.
-#rm -f "${CSD_OUT}"
-
-# Print the filename so callers can figure out the name of the CSD file that was uploaded.
-#echo "${DST_FILE}"
+  # Print the filename so callers can figure out the name of the CSD file that was uploaded.
+  #echo "${PF_BACKUP_OUT#./}"
+  
+else
+  echo "Nothing to archive at the moment"
+fi
