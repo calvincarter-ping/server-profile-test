c2a41d5c9c3ff5443d486af69c46adcd19fc4879
calvincarter <calvincarter.160@gmail.com>
2019-12-26 13:56:08 -0600

    latest

--- pingfederate/hooks/50-before-post-start.sh
+++ pingfederate/hooks/50-before-post-start.sh
@@ -11,7 +11,7 @@
 
 ${VERBOSE} && set -x
 
-echo_green "Before-post-start script running"
-#if test ${RUN_PLAN} = "START" ; then
+echo_green "Before-post-start script running plan ${RUN_PLAN}"
+if test ${RUN_PLAN} = "START" ; then
   run_hook "83-download-csd-s3.sh"
-#fi
+fi
