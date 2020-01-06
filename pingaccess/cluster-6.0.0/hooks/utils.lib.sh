cc0740e599defadeda981be382eb7ad5eff08187
calvincarter <calvincarter.160@gmail.com>
2020-01-06 15:24:33 -0600

    engine add latest

--- pingaccess/cluster-6.0.0/hooks/utils.lib.sh
+++ pingaccess/cluster-6.0.0/hooks/utils.lib.sh
@@ -1,20 +1,72 @@
 #!/usr/bin/env sh
 
-. "${HOOKS_DIR}/pingcommon.lib.sh"
-
-APIATTEMPTS=10
-
+########################################################################################################################
+# Makes curl request to PingAccess API to configure.
+#
+# Arguments
+#   $@ -> The URL and additional needed data to make request
+########################################################################################################################
 function make_api_request
 {
-    local retryAttempts=10
+    curl -k --retry ${API_RETRY_LIMIT} --max-time ${API_TIMEOUT_WAIT} --retry-delay 1 --retry-connrefuse -u Administrator:${INITIAL_ADMIN_PASSWORD} -H "X-Xsrf-Header: PingAccess " "$@"
+    if [[ ! $? -eq 0 ]]; then
+        echo "Admin API connection refused"
+        exit 1
+    fi
+}
+
+########################################################################################################################
+# Makes curl request to PingAccess API to configure.
+#
+# Arguments
+#   $@ -> The URL and additional needed data to make request
+########################################################################################################################
+function make_initial_api_request
+{
+    curl -k --retry ${API_RETRY_LIMIT} --max-time ${API_TIMEOUT_WAIT} --retry-delay 1 --retry-connrefuse -u Administrator:2Access -H "X-Xsrf-Header: PingAccess " "$@"
+    if [[ ! $? -eq 0 ]]; then
+        echo "Admin API connection refused"
+        exit 1
+    fi
+}
+
+########################################################################################################################
+# Makes curl request to localhost PingAccess admin Console heartbeat page.
+# If request fails, wait for 3 seconds and try again.
+#
+# Arguments
+#   N/A
+########################################################################################################################
+function pingaccess_admin_localhost_wait
+{
     while true; do
-        curl -k -u Administrator:2FederateM0re -H "X-Xsrf-Header: PingAccess " "$@"
-        if [[ ! $? -eq 0 && $retryAttempts -gt 0 ]]; then
-            retryAttempts=$((retryAttempts-1))
+        curl -ss --silent -o /dev/null -k https://localhost:9000/pa/heartbeat.ping 
+        if ! test $? -eq 0 ; then
+            echo "Import Config: Server not started, waiting.."
             sleep 3
-        elif [ $retryAttempts -eq 0 ]; then
-            return 1
         else
+            echo "PA started, begin import"
+            break
+        fi
+    done
+}
+
+########################################################################################################################
+# Makes curl request to PingAccess admin cluster heartbeat page.
+# If request fails, wait for 3 seconds and try again.
+#
+# Arguments
+#   N/A
+########################################################################################################################
+function pingaccess_external_engine_wait
+{
+    while true; do
+        curl -ss --silent -o /dev/null -k https://${K8S_STATEFUL_SET_SERVICE_NAME_PA}:9090/pa/heartbeat.ping
+        if ! test $? -eq 0 ; then
+            echo "Adding Engine: Server not started, waiting.."
+            sleep 3
+        else
+            echo "PA started, begin adding engine"
             break
         fi
     done
