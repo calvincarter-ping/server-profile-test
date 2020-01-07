e973fbb3e5cbb73ae05e45eeca62e8bc0a4466ba
calvincarter <calvincarter.160@gmail.com>
2020-01-07 05:21:42 -0600

    removed hardcode keypair

--- pingaccess/cluster-6.0.0/hooks/81-import-initial-configuration.sh
+++ pingaccess/cluster-6.0.0/hooks/81-import-initial-configuration.sh
@@ -42,7 +42,7 @@ OUT=$( make_api_request -X POST -d "{
           \"alias\":\"pingaccess-console\",
           \"organization\":\"Ping Identity\",
           \"validDays\":365,
-          \"commonName\":\"${K8S_STATEFUL_SET_SERVICE_NAME_PINGACCESS_ADMIN}\",
+          \"commonName\":\"${K8S_STATEFUL_SET_SERVICE_NAME_PINGACCESS}\",
           \"country\":\"US\",
           \"signatureAlgorithm\":\"SHA256withRSA\"
         }" https://localhost:9000/pa-admin-api/v3/keyPairs/generate )
