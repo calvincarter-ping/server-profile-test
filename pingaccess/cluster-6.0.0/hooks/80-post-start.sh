addf32dc464f11149dc7c3d1c6657e357e604d43
calvincarter-ping <calvincarter@pingidentity.com>
2020-01-21 16:46:21 -0600

    logic to move pingaccess to s3

--- pingaccess/cluster-6.0.0/hooks/80-post-start.sh
+++ pingaccess/cluster-6.0.0/hooks/80-post-start.sh
@@ -4,10 +4,10 @@
 
 if test ! -z "${OPERATIONAL_MODE}" && \
    test "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE"; then
-    # if test ! -f ${OUT_DIR}/instance/pingaccess_cert_complete_flag; then
-    #   echo "restart logic"
-    # else
-    echo "run plan ${RUN_PLAN}"
-    run_hook "81-import-initial-configuration.sh"
-    # fi
+    if test ! -f ${OUT_DIR}/instance/pingaccess_cert_complete_flag; then
+      run_hook "81-import-initial-configuration.sh"
+    else
+      echo "run plan ${RUN_PLAN}"
+      echo "restart logic"
+    fi
 fi
