4a222a72dcc719f7b83f9f4d0dfe3d2ab056a725
calvincarter-ping <calvincarter@pingidentity.com>
2020-01-22 09:05:14 -0600

    logic to backup

--- pingaccess/cluster-6.0.0/hooks/80-post-start.sh
+++ pingaccess/cluster-6.0.0/hooks/80-post-start.sh
@@ -8,13 +8,13 @@ if test ! -z "${OPERATIONAL_MODE}" && \
     echo "run plan ${RUN_PLAN}"
 
     if test "${RUN_PLAN}" = "START"; then
-
-      echo "run plan ${RUN_PLAN}"
-
       run_hook "81-import-initial-configuration.sh"
       run_hook "82-upload-archive-data-s3.sh"
       SCRIPT=$(ps | grep "${OUT_DIR}/instance/bin/run.sh" | awk '{print $1; exit}')
+
       # Terminate admin to signal a k8s restart
-      #kill -1 "${SCRIPT}"
+      kill -1 "${SCRIPT}"
+    else
+      run_hook "83-download-archive-data-s3"
     fi
 fi
