f1cb9276927d530e67f5bea607e2e42cd919206d
calvincarter-ping <calvincarter@pingidentity.com>
2020-01-22 01:36:03 -0600

    logic to backup

--- pingaccess/cluster-6.0.0/hooks/80-post-start.sh
+++ pingaccess/cluster-6.0.0/hooks/80-post-start.sh
@@ -6,7 +6,7 @@ if test ! -z "${OPERATIONAL_MODE}" && \
    test "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE"; then
 
     echo "run plan ${RUN_PLAN}"
-    
+
     if test "${RUN_PLAN}" = "START"; then
 
       echo "run plan ${RUN_PLAN}"
@@ -14,6 +14,6 @@ if test ! -z "${OPERATIONAL_MODE}" && \
       run_hook "81-import-initial-configuration.sh"
       run_hook "82-upload-archive-data-s3.sh"
       # Terminate admin to signal a k8s restart
-      kill -1 $(ps | grep "${OUT_DIR}/instance/bin/run.sh" | awk '{print $1}')
+      kill $(ps | grep "${OUT_DIR}/instance/bin/run.sh" | awk '{print $1}')
     fi
 fi
--- pingaccess/cluster-6.0.0/hooks/utils.lib.sh
+++ pingaccess/cluster-6.0.0/hooks/utils.lib.sh
@@ -6,7 +6,7 @@
 # Arguments
 #   $@ -> The URL and additional needed data to make request
 ########################################################################################################################
-function make_api_request
+function make_api_request()
 {
     curl -k --retry ${API_RETRY_LIMIT} --max-time ${API_TIMEOUT_WAIT} --retry-delay 1 --retry-connrefuse -u Administrator:${INITIAL_ADMIN_PASSWORD} -H "X-Xsrf-Header: PingAccess " "$@"
     if [[ ! $? -eq 0 ]]; then
@@ -21,7 +21,7 @@ function make_api_request
 # Arguments
 #   $@ -> The URL and additional needed data to make request
 ########################################################################################################################
-function make_initial_api_request
+function make_initial_api_request()
 {
     curl -k --retry ${API_RETRY_LIMIT} --max-time ${API_TIMEOUT_WAIT} --retry-delay 1 --retry-connrefuse -u Administrator:2Access -H "X-Xsrf-Header: PingAccess " "$@"
     if [[ ! $? -eq 0 ]]; then
@@ -37,7 +37,7 @@ function make_initial_api_request
 # Arguments
 #   N/A
 ########################################################################################################################
-function pingaccess_admin_localhost_wait
+function pingaccess_admin_localhost_wait()
 {
     while true; do
         curl -ss --silent -o /dev/null -k https://localhost:9000/pa/heartbeat.ping 
@@ -58,7 +58,7 @@ function pingaccess_admin_localhost_wait
 # Arguments
 #   N/A
 ########################################################################################################################
-function pingaccess_external_engine_wait
+function pingaccess_external_engine_wait()
 {
     while true; do
         curl -ss --silent -o /dev/null -k https://${K8S_STATEFUL_SET_SERVICE_NAME_PINGACCESS_ADMIN}:9090/pa/heartbeat.ping
@@ -70,4 +70,26 @@ function pingaccess_external_engine_wait
             break
         fi
     done
+}
+
+########################################################################################################################
+# Function to install AWS command line tools
+#
+# Arguments
+#   N/A
+########################################################################################################################
+function installTools()
+{
+   if [ -z "$(which aws)" ]; then
+      #   
+      #  Install AWS platform specific tools
+      #
+      echo "Installing AWS CLI tools for S3 support"
+      #
+      # TODO: apk needs to move to the Docker file as the package manager is plaform specific
+      #
+      apk --update add python3
+      pip3 install --no-cache-dir --upgrade pip
+      pip3 install --no-cache-dir --upgrade awscli
+   fi
 }
