e69998b8ad23234a97648e7d3a7cbce5f691f9d1
calvincarter-ping <calvincarter@pingidentity.com>
2020-01-22 09:56:58 -0600

    logic to backup

--- pingaccess/cluster-6.0.0/hooks/80-post-start.sh
+++ pingaccess/cluster-6.0.0/hooks/80-post-start.sh
@@ -1,20 +1,35 @@
 #!/usr/bin/env sh
 
 . "${HOOKS_DIR}/pingcommon.lib.sh"
+. "${HOOKS_DIR}/utils.lib.sh"
 
-if test ! -z "${OPERATIONAL_MODE}" && \
-   test "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE"; then
+if test ! -z "${OPERATIONAL_MODE}" && test "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE"; then
 
-    echo "run plan ${RUN_PLAN}"
+  installTools
 
-    if test "${RUN_PLAN}" = "START"; then
-      run_hook "81-import-initial-configuration.sh"
-      run_hook "82-upload-archive-data-s3.sh"
-      SCRIPT=$(ps | grep "${OUT_DIR}/instance/bin/run.sh" | awk '{print $1; exit}')
+  BUCKET_URL_NO_PROTOCOL=${BACKUP_URL#s3://}
+  BUCKET_NAME=$(echo ${BUCKET_URL_NO_PROTOCOL} | cut -d/ -f1)
+  DIRECTORY_NAME=$(echo ${PING_PRODUCT} | tr '[:upper:]' '[:lower:]')
 
-      # Terminate admin to signal a k8s restart
-      kill -1 "${SCRIPT}"
-    else
-      run_hook "83-download-archive-data-s3"
-    fi
+  if test "${BACKUP_URL}" == */pingaccess; then
+    TARGET_URL="${BACKUP_URL}"
+  else
+    TARGET_URL="${BACKUP_URL}/${DIRECTORY_NAME}"
+  fi
+  CERTFLAG="${TARGET_URL}/pingaccess_cert_complete_flag"
+
+  RESULT="$(aws s3 ls ${CERTFLAG} > /dev/null 2>&1;echo $?)"
+  if test "${RESULT}" = "0"; then
+    run_hook "83-download-archive-data-s3"
+  elif test "${RESULT}" = "1" || test "${RESULT}" = "2"; then
+    echo "error occured"
+  else
+    run_hook "81-import-initial-configuration.sh"
+    run_hook "82-upload-archive-data-s3.sh"
+    SCRIPT=$(ps | grep "${OUT_DIR}/instance/bin/run.sh" | awk '{print $1; exit}')
+
+    # Terminate admin to signal a k8s restart
+    kill -1 "${SCRIPT}"
+  fi
+
 fi
