2fb99dd8879405cecafa66b600d0bd48d9693343
calvincarter-ping <calvincarter@pingidentity.com>
2020-01-22 01:20:03 -0600

    logic to backup

--- pingaccess/cluster-6.0.0/hooks/80-post-start.sh
+++ pingaccess/cluster-6.0.0/hooks/80-post-start.sh
@@ -4,7 +4,13 @@
 
 if test ! -z "${OPERATIONAL_MODE}" && \
    test "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE"; then
+
+    echo "run plan ${RUN_PLAN}"
+    
     if test "${RUN_PLAN}" = "START"; then
+
+      echo "run plan ${RUN_PLAN}"
+
       run_hook "81-import-initial-configuration.sh"
       run_hook "82-upload-archive-data-s3.sh"
       # Terminate admin to signal a k8s restart
